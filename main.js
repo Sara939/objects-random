39c65671945bfe9ae23dccee261ce28a0e711c63
sara-likaount <saralikaunt@gmail.com>
2022-05-26 09:22:19 +0300

    m

---
+++ main.js
@@ -0,0 +1,58 @@
+// 35:
+// function rannum(){
+//     console.log(Math.round(Math.random()*11));
+// }
+// rannum();
+
+// 36:
+
+// function randomusernum(num){
+//     console.log(Math.round(Math.random()*num+1));
+// }
+// randomusernum(20);
+
+// 37-8:
+
+// function usersnum(){
+//     var number= +prompt("num");
+//     var selected= Math.round(Math.random()*11);
+//     if(selected ==number){
+//         console.log("succes")
+//     }
+//     else{console.log("fail")
+// }
+// }
+// usersnum();
+
+// 39:
+// function pro(){
+//     var myarray= [];
+//     for(i=0; i<=10;i++){
+//     myarray[i]= Math.round(Math.random()*101);
+// }
+// console.log(myarray)
+// }
+// pro();
+
+// 40:
+// function getnum(){
+//     var luckynum= +prompt("num");
+//     var selected= Math.round(Math.random()*11);
+//     console.log(Math.max(luckynum,selected));
+// }
+// getnum();
+
+// 41://///////////////////////////////////////////////////////////////
+// function getmin(){
+//     var lowst;
+//     for(i=0; i<5; i++){
+//     var luckynum= +prompt("num");
+//         if(luckynum[i]< lowst){
+//         lowst= luckynum[i];
+//         console.log(luckynum[i]);
+// }
+// // console.log(lowst);
+// }
+// }
+// getmin();
+
